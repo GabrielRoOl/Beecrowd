aa5c98a29b835f763ff1a31d6e340c18754038e5
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-10-16 07:44:39 -0300

    first commit, 1000, 1001, 1002, 1003, 1004, 1005

---
+++ src/exercicios/ExtremamenteBásico.java
@@ -0,0 +1,28 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExtremamenteBásico {
+	public static void main(String[] args) {
+		/*
+		 * Leia 2 valores inteiros e armazene-os nas variáveis A e B. Efetue a soma de A
+		 * e B atribuindo o seu resultado na variável X. Imprima X conforme exemplo
+		 * apresentado abaixo. Não apresente mensagem alguma além daquilo que está sendo
+		 * especificado e não esqueça de imprimir o fim de linha após o resultado, caso
+		 * contrário, você receberá "Presentation Error".
+		 * 
+		 * Entrada A entrada contém 2 valores inteiros.
+		 * 
+		 * Saída Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da
+		 * variável X e pelo final de linha. Cuide para que tenha um espaço antes e
+		 * depois do sinal de igualdade, conforme o exemplo abaixo.
+		 */
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		int soma = x + y;
+		System.out.println("X = " + soma);
+	}
+}
