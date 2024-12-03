4fa7f17a69cb53e152b9e05ebf404ec575e00673
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-03 14:15:35 -0300

    1004 - Produto Simples

--- src/beecrowd/iniciante/Soma_Simples.java
+++ src/beecrowd/iniciante/Soma_Simples.java
@@ -1,9 +1,18 @@
 package beecrowd.iniciante;
 
+import java.util.Scanner;
+
 public class Soma_Simples {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+		int A, B, SOMA;
+		A = sc.nextInt();
+		B = sc.nextInt();
+
+		SOMA = A + B;
+		System.out.println("SOMA = " + SOMA);
 
 	}
 
