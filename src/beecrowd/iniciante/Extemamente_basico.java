d9d3afdfb1a43b9daa8167a5f432b7f45404f553
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-03 13:25:27 -0300

    Extremamente Básico

---
+++ src/beecrowd/iniciante/Extemamente_basico.java
@@ -0,0 +1,18 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class Extemamente_basico {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+
+		int soma = x + y;
+		System.out.println("X = " + soma);
+
+	}
+
+}
