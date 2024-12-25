e75fd1dea1f305794bd1fae443be7627b3efd2e2
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2024-12-25 13:02:17 -0300

    1022 - Update

--- src/beecrowd/iniciante/TDA_Racional.java
+++ src/beecrowd/iniciante/TDA_Racional.java
@@ -3,21 +3,25 @@ package beecrowd.iniciante;
 import java.util.Scanner;
 
 public class TDA_Racional {
+	
+	public static double soma(int n1, int d1, int d2, int n2) {
+		return (n1 * d2 + n2 * d1) / (d1 * d2);
+	}
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 
 		String entr = sc.nextLine();
 		String[] vetor = entr.split(" ");
-		
+
 		int N1 = Integer.parseInt(vetor[0]);
-		int D2 = Integer.parseInt(vetor[2]);
+		int D1 = Integer.parseInt(vetor[2]);
+		int N2 = Integer.parseInt(vetor[6]);
+		int D2 = Integer.parseInt(vetor[4]);
+
+		System.out.println((double)soma(N1, D1, N2, D2));
 		
-		
-		for (String p : vetor) {
-			System.out.println(p);
-		}
 
 	}
 
