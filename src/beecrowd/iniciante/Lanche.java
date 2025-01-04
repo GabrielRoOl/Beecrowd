70cf64c465ac704d57060362d0ffff7eb2364fae
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2025-01-04 12:03:30 -0300

    1038 - Lanche

--- src/beecrowd/iniciante/Lanche.java
+++ src/beecrowd/iniciante/Lanche.java
@@ -1,9 +1,39 @@
 package beecrowd.iniciante;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Lanche {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		int p = sc.nextInt();
+		int qtd = sc.nextInt();
+
+		double pedido = 0;
+
+		switch (p) {
+		case 1:
+			pedido = qtd * 4.0;
+			break;
+		case 2:
+			pedido = qtd * 4.5;
+			break;
+		case 3:
+			pedido = qtd * 5.0;
+			break;
+		case 4:
+			pedido = qtd * 2.0;
+			break;
+		case 5:
+			pedido = qtd * 1.5;
+			break;
+		default:
+			break;
+		}
+		System.out.println("Total: R$ " + String.format("%.2f", pedido));
 
 	}
 
