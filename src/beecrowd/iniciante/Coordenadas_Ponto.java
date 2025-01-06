86235b2791941d2ffd73d1d40a671d0ae01ac093
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2025-01-06 13:12:31 -0300

    1041 - Coordenadas de um Ponto

--- src/beecrowd/iniciante/Coordenadas_Ponto.java
+++ src/beecrowd/iniciante/Coordenadas_Ponto.java
@@ -8,7 +8,27 @@ public class Coordenadas_Ponto {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+		
+		double x = sc.nextDouble();
+		double y = sc.nextDouble();
+		
 
+		if(x > 0.0 && y > 0.0) {
+			System.out.println("Q1");
+		} else if(x > 0.0 && y < 0.0) {
+			System.out.println("Q4");
+		} else if(x < 0.0 && y > 0.0) {
+			System.out.println("Q2");
+		} else if(x < 0.0 && y < 0.0) {
+			System.out.println("Q3");
+		} else if( x == 0.0 && y == 0.0) {
+			System.out.println("Origem");
+		} else if(x == 0.0 && (y > 0.0 || y < 0.0)) {
+			System.out.println("Eixo X");
+		} else if(y == 0.0 && (x < 0.0 || x > 0.0)) {
+			System.out.println("Eixo Y");
+		}
+		sc.close();
 	}
 
 }
