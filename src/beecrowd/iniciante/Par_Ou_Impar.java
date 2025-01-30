bf43a4058f9ba2ee1a10ab6673cbe1c17b753ece
GabrielRoOl <gabriel.ro.ol@hotmail.com>
2025-01-30 16:13:51 -0300

    1074 - Par ou Ímpar

--- src/beecrowd/iniciante/Par_Ou_Impar.java
+++ src/beecrowd/iniciante/Par_Ou_Impar.java
@@ -1,9 +1,36 @@
 package beecrowd.iniciante;
 
+import java.util.Scanner;
+
 public class Par_Ou_Impar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		
+		while (N > 0) {
+			
+			int x = sc.nextInt();
+			if (x == 0) {
+				System.out.println("NULL");
+			} else if(x % 2 == 0){
+				System.out.print("EVEN ");
+				if(x > 0){
+					System.out.println("POSITIVE");
+				}else{
+					System.out.println("NEGATIVE");
+				}
+			} else{
+				System.out.print("ODD ");
+				if(x > 0){
+					System.out.println("POSITIVE");
+				} else{
+					System.out.println("NEGATIVE");
+				}
+			}
+			N--;
+		}
 
 	}
 
