9ec6cc36f04813b13c9eb363b378876aee3174f6
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-01 07:28:58 -0300

    Update

--- src/beecrowd/iniciante/Par_Ou_Impar.java
+++ src/beecrowd/iniciante/Par_Ou_Impar.java
@@ -1,9 +1,24 @@
 package beecrowd.iniciante;
 
+import java.util.Scanner;
+
 public class Par_Ou_Impar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+		while(N > 0) {
+			int X = sc.nextInt();
+			if(X % 2 != 0) {
+				
+			}
+			
+			
+			N--;
+		}
+		
 
 	}
 
