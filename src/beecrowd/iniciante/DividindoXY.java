00b3b9200a3a7b20aaef006239c4bddb0ad38ce3
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-09 13:58:13 -0300

    1116 - Dividindo X por Y

---
+++ src/beecrowd/iniciante/DividindoXY.java
@@ -0,0 +1,28 @@
+package beecrowd.iniciante;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class DividindoXY {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		double div = 1;
+		int N = sc.nextInt();
+		
+		for(int i = 0; i < N; i++) {
+			int X = sc.nextInt();
+			int Y = sc.nextInt();
+			if(X < 0) {
+				System.out.println("divisao impossivel");
+			}else {
+				div = X / Y;
+				System.out.println(String.format("%.1f", div));
+			}
+		}
+
+	}
+
+}
