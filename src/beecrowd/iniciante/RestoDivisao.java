88151d715af4eddd110b98ace94d9681c053be23
Gabriel Rodrigues <144238400+GabrielRoOl@users.noreply.github.com>
2025-02-11 13:11:46 -0300

    1133 - Resto da Divisão

---
+++ src/beecrowd/iniciante/RestoDivisao.java
@@ -0,0 +1,27 @@
+package beecrowd.iniciante;
+
+import java.util.Scanner;
+
+public class RestoDivisao {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int Y = sc.nextInt();
+		
+		int maior = X > Y ? X : Y;
+		int menor = X < Y ? X : Y;
+		
+		for(int i= menor; i < maior; i++) {
+			if(i % 5 == 3) {
+				System.out.println(i);
+			}
+			if(i % 5 == 2) {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
